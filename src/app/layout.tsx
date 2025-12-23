180af705bb23a144e374db83af6620b47373438d
thipwarin <prae.tipppy@gmail.com>
2025-12-23 11:10:26 +0700

    Set project font to K2D globally for both Owners and Admin.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { K2D } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const k2d = K2D({
+  weight: ["300", "400", "500", "600", "700"],
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-k2d",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${k2d.variable} antialiased`}
       >
         {children}
       </body>
